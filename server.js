c6312dd30b651a992fcc501e5b3510daecd20bad
hatimabualrub <64695576+hatimabualrub@users.noreply.github.com>
2020-12-11 18:27:08 +0200

    Some Fixes2

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const productRouter = require("./routers/productRouter.js");
 const userRouter = require("./routers/userRouter.js");
@@ -12,11 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 port = process.env.PORT || 5000;
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.use(cors());
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
